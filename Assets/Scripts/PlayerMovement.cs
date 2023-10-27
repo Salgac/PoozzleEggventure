fd3f4923ef6c8ccae8c0b4736004732571ffe098
Patrik Lukáčik <55752483+Palapuk@users.noreply.github.com>
2023-10-27 17:25:40 +0200

    Desired Egg movement - no collisions
     - trosku spagety
     - bude potrebovat refaktoring

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,56 +6,104 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float speed = 1.0f;
+    private float rotation_speed = 1.0f;
     private float movingPercentage = 0.0f;
     private float correctionDelta;
-    private Vector3 direction = new Vector3(0, 0, 0);
 
-    private List<Vector3> KeyBuffer= new List<Vector3>();
+    private Boolean standing = true;
+    private Vector3 direction;
+    private KeyCode StandUpKey;
 
+    private List<KeyCode> KeyBuffer = new List<KeyCode>();
 
     // Update is called once per frame
     void Update()
     {
         
         // set direction vector
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.W))
         {
-            KeyBuffer.Add(new Vector3(0, 0, 1.0f));
+            KeyBuffer.Add(KeyCode.W);
         }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            KeyBuffer.Add(new Vector3(0, 0, -1.0f));
+            KeyBuffer.Add(KeyCode.S);
         }
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            KeyBuffer.Add(new Vector3(-1.0f, 0, 0));
+            KeyBuffer.Add(KeyCode.A);
         }
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (Input.GetKeyDown(KeyCode.D))
         {
-            KeyBuffer.Add(new Vector3(1.0f, 0, 0));
+            KeyBuffer.Add(KeyCode.D);
         }
-        // Testing
-        // rotations - in testing
-        if (Input.GetKeyDown(KeyCode.Q))
+        
+        // 2 types of movement
+        if (standing && KeyBuffer.Count > 0)
+        {   
+            LayDown();
+        }
+        if (!standing && KeyBuffer.Count > 0)
         {
-            transform.Rotate(transform.localRotation.x - 90,0,0);
+            // decide whether to roll or stand up
+            Boolean AcceptKey = false;
+            Vector3 state = transform.rotation.eulerAngles;
+            if (state.z != 0 && (KeyBuffer[0] == KeyCode.W || KeyBuffer[0] == KeyCode.S))
+            {
+                Roll();
+                AcceptKey = true;
+            }
+                
+            if (state.x != 0 && (KeyBuffer[0] == KeyCode.A || KeyBuffer[0] == KeyCode.D))
+            {
+                Roll();
+                AcceptKey = true;
+            }
+            if (!AcceptKey)
+            {
+                if (StandUp())
+                    AcceptKey = true;
+            }
+                
+
+
+            if (!AcceptKey) KeyBuffer.Clear();
         }
 
-        // moving
+        
+
+    }
+    private void Roll()
+    {
+        Debug.Log(KeyBuffer.Count);
         if (KeyBuffer.Count > 0)
         {
-            direction = KeyBuffer[0];
+            switch (KeyBuffer[0])
+            {
+                case (KeyCode.W):
+                    direction = new Vector3(0,0,1.0f);
+                    break;
+                case (KeyCode.S):
+                    direction = new Vector3(0, 0, -1.0f);
+                    break;
+                case (KeyCode.A):
+                    direction = new Vector3(-1.0f, 0, 0);
+                    break;
+                case (KeyCode.D):
+                    direction = new Vector3(1.0f, 0, 0);
+                    break;
+            }
             if (movingPercentage + Time.deltaTime >= 1.0f)
             {
                 correctionDelta = 1.0f - movingPercentage;
                 transform.position = transform.position + speed * direction * correctionDelta;
-                movingPercentage += Time.deltaTime;
+                movingPercentage += Time.deltaTime * speed;
             }
             else
             {
-                movingPercentage += Time.deltaTime;
+                movingPercentage += Time.deltaTime * speed;
                 transform.position = transform.position + speed * direction * Time.deltaTime;
-            } 
+            }
         }
         if (movingPercentage >= 1.0f)
         {
@@ -67,7 +115,42 @@ public class PlayerMovement : MonoBehaviour
             float z = Convert.ToInt32(transform.position.z * 10) / 10.0f;
             transform.position = new Vector3(x, y, z);
         }
-        
-
     }
+
+    private void LayDown()
+    {
+        switch (KeyBuffer[0])
+        {
+            case (KeyCode.W):
+                StandUpKey = KeyCode.S; 
+                transform.Rotate(90,0,0);
+                break;
+            case (KeyCode.S):
+                StandUpKey = KeyCode.W;
+                transform.Rotate(-90, 0, 0);
+                break;
+            case (KeyCode.A):
+                StandUpKey = KeyCode.D;
+                transform.Rotate(0, 0, 90);
+                break;
+            case (KeyCode.D):
+                StandUpKey = KeyCode.A;
+                transform.Rotate(0, 0, -90);
+                break;
+        }
+        standing = false;
+        KeyBuffer.RemoveAt(0);
+    }
+    private Boolean StandUp()
+    {
+        if (KeyBuffer[0] == StandUpKey) {
+            transform.rotation = Quaternion.identity;
+            standing = true;
+            KeyBuffer.RemoveAt(0);
+            return true;
+        }
+        return false;
+        
+    }
+
 }
