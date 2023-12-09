d4477efb5987e645d513124c1b4cc06534db5d08
Patrik Lukáčik <55752483+Palapuk@users.noreply.github.com>
2023-12-09 22:10:05 +0100

    Refactoring + comments

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+ /*
+  * Class handling player movement
+  */
 public class PlayerMovement : MonoBehaviour
 {
+    // Movement variables
     private float speed = 2.0f;
 
     private bool standing = true;
@@ -18,17 +22,21 @@ public class PlayerMovement : MonoBehaviour
 
     private List<KeyCode> KeyBuffer = new List<KeyCode>();
 
+    // Animation variables
     public float transitionTime = 0.5f;
     private bool isTransitioning = false;
 
+    // References variables
     public GameObject thiccAssVajicko;
     private MovementController movementBar;
     private List<GameObject> floorList;
 
+    // Teleport variables
     public bool NeedToTeleport = false;
     public Vector3 TeleportTo;
     public int DisabledPortal = 0;
 
+    // Set all needed variables - finding objects...
     void Start()
     {
         movementBar = FindObjectOfType<MovementController>();
@@ -46,7 +54,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {    
-        // set direction vector
+        // set direction vector based on input and add to key buffer
         if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             KeyBuffer.Add(KeyCode.W);
@@ -63,8 +71,10 @@ public class PlayerMovement : MonoBehaviour
         {
             KeyBuffer.Add(KeyCode.D);
         }
-        
+
+        // Perform animation
         if (!isTransitioning) {
+            // Teleport
             if (NeedToTeleport)
             {
                 transform.position = TeleportTo;
@@ -73,6 +83,7 @@ public class PlayerMovement : MonoBehaviour
                 DisabledPortal = 1;
             }
             // 2 types of movement
+            // Lay down
             if (standing && KeyBuffer.Count > 0)
             {
                 lastPosition = transform.position;
@@ -85,6 +96,7 @@ public class PlayerMovement : MonoBehaviour
                 // decide whether to roll or stand up
                 Boolean AcceptKey = false;
                 Vector3 state = transform.rotation.eulerAngles;
+                // Perform roll animation
                 if (state.z != 0 && (KeyBuffer[0] == KeyCode.W || KeyBuffer[0] == KeyCode.S))
                 {
                     lastPosition = transform.position;
@@ -93,7 +105,7 @@ public class PlayerMovement : MonoBehaviour
                     StartCoroutine(Rolling());
                     AcceptKey = true;
                 }
-                    
+                // perform roll animation    
                 if (state.x != 0 && (KeyBuffer[0] == KeyCode.A || KeyBuffer[0] == KeyCode.D))
                 {
                     lastPosition = transform.position;
@@ -102,8 +114,10 @@ public class PlayerMovement : MonoBehaviour
                     StartCoroutine(Rolling());
                     AcceptKey = true;
                 }
+                // If no key is accepted then key buffer is reseted
                 if (!AcceptKey)
                 {
+                    // Try to stand up
                     if (KeyBuffer[0] == StandUpKey)
                     {
                         lastPosition = transform.position;
@@ -121,8 +135,10 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // Rolling animation
     private IEnumerator Rolling()
     {
+        // Prepare variables
         Vector3 startPosition = transform.position;
         Vector3 endPosition = startPosition;
         Quaternion startEggRotation = thiccAssVajicko.transform.rotation;
@@ -130,6 +146,7 @@ public class PlayerMovement : MonoBehaviour
 
         isTransitioning = true;
         
+        // Calculate final position and rotation
         if (KeyBuffer.Count > 0)
         {
             switch (KeyBuffer[0])
@@ -156,12 +173,16 @@ public class PlayerMovement : MonoBehaviour
                     break;
             }
             thiccAssVajicko.transform.rotation = startEggRotation;
+
+            // Coroutine cycle
             for (float t = 0.0f; t < 1.0f; t += Time.deltaTime * speed)
             {
                 transform.position = Vector3.Lerp(startPosition, endPosition, t);
                 thiccAssVajicko.transform.rotation = Quaternion.Lerp(startEggRotation, endEggRotation, t);
                 yield return null;
             }
+
+            // Reset variables
             isTransitioning = false;
             transform.position = endPosition;
             DisabledPortal = 0;
@@ -169,14 +190,17 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // Lay down animation
     private IEnumerator LayDown()
     {
+        // Prepare variables
         isTransitioning = true;
         Vector3 startPosition = transform.position;
         Quaternion startRotation = transform.rotation;
         Vector3 endPosition = startPosition;
         Quaternion endRotation = Quaternion.identity;
 
+        // Calculate final position
         switch (KeyBuffer[0])
         {
             case KeyCode.W:
@@ -201,6 +225,7 @@ public class PlayerMovement : MonoBehaviour
                 break;
         }
 
+        // Coroutine cycle
         for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / transitionTime)
         {
             transform.position = Vector3.Lerp(startPosition, endPosition, t);
@@ -208,6 +233,7 @@ public class PlayerMovement : MonoBehaviour
             yield return null;
         }
 
+        // Reset variables
         transform.position = endPosition;
         transform.rotation = endRotation;
         standing = false;
@@ -220,14 +246,20 @@ public class PlayerMovement : MonoBehaviour
         CheckUnder();
         DisabledPortal = 0;
     }
+
+    // Stand up coroutine
     private IEnumerator StandUp()
     {
         isTransitioning = true;   
         if (KeyBuffer[0] == StandUpKey) {
+
+            // Prepare variables
             Vector3 startPosition = transform.position;
             Quaternion startRotation = transform.rotation;
             Vector3 endPosition = startPosition;
             Quaternion endRotation = Quaternion.identity;   
+
+            // Calculate final postion
             switch (KeyBuffer[0])
             {
                 case KeyCode.W:
@@ -244,6 +276,7 @@ public class PlayerMovement : MonoBehaviour
                     break;
             }
             
+            // Coroutine cycle
             for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / transitionTime)
             {
                 transform.position = Vector3.Lerp(startPosition, endPosition, t);
@@ -251,6 +284,7 @@ public class PlayerMovement : MonoBehaviour
                 yield return null;
             }
 
+            // Reset variables
             transform.position = endPosition;
             transform.rotation = endRotation;
             standing = true;
@@ -266,8 +300,10 @@ public class PlayerMovement : MonoBehaviour
         yield return false;
     }
 
+    // Return to last position Coroutine
     private IEnumerator ReturnToLastPosition()
     {
+        // Variable set
         isTransitioning = true;
         Vector3 startPosition = transform.position;
         Quaternion startRotation = transform.rotation;
@@ -289,6 +325,7 @@ public class PlayerMovement : MonoBehaviour
             yield return null;
         }
 
+        // Set final variables after animation
         isTransitioning = false;
         transform.position = endPosition;
         transform.rotation = endRotation;
@@ -296,30 +333,31 @@ public class PlayerMovement : MonoBehaviour
         KeyBuffer.Clear();
     }
 
+    // Object collisions with triggers
     public void OnTriggerEnter(Collider other)
     {
         // check for collidable objects
         if (!other.CompareTag("Collidable"))
-        {
-            
+        {       
             return;
         }
-        // LayDown into object hotfix? or Fix?
+        // Check layer of the object
         if (other.gameObject.transform.position.y >= 1)
         {
             Debug.Log(other.gameObject.name);
+            // Check for active animations
             if (isTransitioning)
             {
+                // Stop animation
                 StopAllCoroutines();
                 movementBar.IncrementProgress();
                 isTransitioning = false;
             }
 
+            // Return to last position before collision
             StartCoroutine(ReturnToLastPosition());
-            // transform.position = lastPosition;
-            // transform.rotation = Quaternion.identity;
-            // transform.Rotate(lastRotation);
 
+            // Reset standing variable
             if (lastRotation == new Vector3(0, 0, 0))
             {
                 standing = true;
@@ -333,6 +371,7 @@ public class PlayerMovement : MonoBehaviour
         } 
     }
 
+    // Restart current scene
     public IEnumerator Reset()
     {
         for (float t = 0.0f; t < 1.0f; t += Time.deltaTime * speed)
@@ -342,11 +381,13 @@ public class PlayerMovement : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
+    // Checking tile under the egg
     private void CheckUnder()
     {
-
+        // FInding tile game object under player
         var under = floorList.Where(i => (i.transform.position.x == transform.position.x && i.transform.position.z == transform.position.z)).ToArray();
-        Debug.Log(under.Length);
+        
+        // If no tile is discovered under player drop the egg     
         if (under.Length <= 0)
         {
             StopAllCoroutines();
