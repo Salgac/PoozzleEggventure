174535a9898d35aee72f73b430e243052a3d93ff
Salgac <domco.salgovic@gmail.com>
2023-12-17 21:13:07 +0100

    Tweak number of levels

--- Assets/Scripts/MainMenu/LevelsButtons.cs
+++ Assets/Scripts/MainMenu/LevelsButtons.cs
@@ -17,7 +17,7 @@ public class LevelsButtons : MonoBehaviour
 
         //instantiate button for every level
         int n = 1;
-        for (int i = 0; i < 4; i++)
+        for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 5; j++)
             {
