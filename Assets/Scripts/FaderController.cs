3ddeb953b2c7ef20cb84647768f2fe84550b2ab7
Patrik Lukáčik <55752483+Palapuk@users.noreply.github.com>
2023-12-17 16:31:27 +0100

    added audio fade out

--- Assets/Scripts/FaderController.cs
+++ Assets/Scripts/FaderController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +8,13 @@ public class FaderController : MonoBehaviour
     public Animator animator;
     private string levelToLoad;
 
+    public AudioSource audioSource;
+
     // Start is called before the first frame update
-    void Start() { }
+    void Start() {
+        audioSource = GameObject.FindFirstObjectByType<AudioSource>();
+
+    }
 
     // Update is called once per frame
     void Update() { }
@@ -17,12 +23,37 @@ public class FaderController : MonoBehaviour
     {
         this.levelToLoad = levelToLoad;
         animator.SetTrigger("FadeOut");
+        try
+        {
+            StartCoroutine(audioFadeOut());
+        }
+        catch { }
+        
     }
 
     public void OnFadeComplete()
     {
         // fallback to main menu - TODO
+        if (audioSource)
+            audioSource.Stop();
         Debug.Log(SceneManager.GetSceneByName(levelToLoad).IsValid());
         SceneManager.LoadScene(levelToLoad);
+    }
+
+    public IEnumerator audioFadeOut()
+    {
+        if (audioSource)
+        {
+            float startVolume = audioSource.volume;
+
+            while (audioSource.volume > 0)
+            {
+                audioSource.volume -= startVolume * Time.deltaTime / 1.0f;
+
+                yield return null;
+            }
+            audioSource.volume = startVolume;
+        }
+        
     }
 }
